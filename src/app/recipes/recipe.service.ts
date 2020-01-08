d3a4155f9b63e15b39f1b4bd6d57bdac86214fc2
Joe Giuffrida <giuffridajoe@gmail.com>
2020-01-08 07:04:24 -0500

    updates

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,11 +1,27 @@
 import { Recipe } from './recipe.model';
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is a test recipe', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
-        new Recipe('Another Test Recipe', 'This is a test recipe', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg')
+        new Recipe(
+            'A Test Recipe',
+            'This is a test recipe',
+            'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
+            [
+                new Ingredient('Meat', 1),
+                new Ingredient('French Fires', 20)
+            ]),
+
+        new Recipe(
+            'Another Test Recipe',
+            'This is a test recipe',
+            'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
+            [
+                new Ingredient('Bread', 1),
+                new Ingredient('Meat', 2)
+            ])
       ];
 
     getRecipes() {
